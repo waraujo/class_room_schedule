6572b0ec1d245f658baf1f33d79e5343e7187371
Waldyr Guimarães Araújo de Souza <w-araujo@live.com>
2019-10-02 18:22:40 +0200

    Update institutions.rb

--- scheduler/spec/factories/institutions.rb
+++ scheduler/spec/factories/institutions.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do 
-	factory :intitution do 
+	factory :institution do 
 		sequence(:name) { |n| "Institution :: |#{n}|"}
 		max_repeted_class 2
 
@@ -7,8 +7,8 @@ FactoryGirl.define do
 			series_count 5
 		end
 
-		after(:create) do |instituiton, evaluator| 
-			create_list(:serie, evaluator.series_count, instituiton: instituiton)
+		after(:create) do |institution, evaluator| 
+			create_list(:serie, evaluator.series_count, institution: institution)
 		end
 	end
 end
